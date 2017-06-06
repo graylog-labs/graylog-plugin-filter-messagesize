a986408311133d8486bdd3fe204471e0be40a414
Lennart Koopmann <lennart@graylog.com>
2017-06-06 14:37:14 -0500

    bump

--- src/main/java/org/graylog/plugins/messagesizefilter/MessageSizeFilterMetaData.java
+++ src/main/java/org/graylog/plugins/messagesizefilter/MessageSizeFilterMetaData.java
@@ -33,7 +33,7 @@ public class MessageSizeFilterMetaData implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return Version.fromPluginProperties(getClass(), PLUGIN_PROPERTIES, "version", Version.from(0, 0, 1));
+        return Version.fromPluginProperties(getClass(), PLUGIN_PROPERTIES, "version", Version.from(0, 0, 2));
     }
 
     @Override
